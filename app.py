eeacabb2a5b3c43f0c88649ff9ca0a22dbceec91
htglvl <htglvl@gmail.com>
2024-06-06 17:47:09 +0700

    wtf loi dong comment????? Anyway, done multiple user? maybe???
    Signed-off-by: htglvl <htglvl@gmail.com>

--- app.py
+++ app.py
@@ -21,18 +21,32 @@ if not os.path.exists("uploads"):
     os.makedirs("uploads")
 
 
-if not os.path.exists("uploads/{now}"):
-    os.makedirs(f"uploads/{now}")
+# if not os.path.exists("uploads/{now}"):
+#     os.makedirs(f"uploads/{now}")
 
-if not os.path.exists("processed/{now}"):
-    os.makedirs(f"processed/{now}")
+# if not os.path.exists("processed/{now}"):
+#     os.makedirs(f"processed/{now}")
 
-upload_path = 'uploads/' + now
-process_path = 'processed/'+now
-archived_path = 'archived/'+ now
+# upload_path = 'uploads/' + now
+# process_path = 'processed/'+now
+archived_path = 'archived/'
 
-app.config['UPLOAD_FOLDER'] = upload_path
-app.config['PROCESSED_FOLDER'] = process_path
+def create_folder_and_update_path():
+    now = datetime.now()
+    now = now.strftime("%Y%m%d%H%M%S-%f")
+    if not os.path.exists("uploads/{now}"):
+        os.makedirs(f"uploads/{now}")
+    if not os.path.exists("processed/{now}"):
+        os.makedirs(f"processed/{now}")
+    upload_path = 'uploads/' + now
+    process_path = 'processed/'+now
+    archived_path = 'archived/'+ now
+    return now, upload_path, process_path
+
+
+
+# app.config['UPLOAD_FOLDER'] = upload_path
+# app.config['PROCESSED_FOLDER'] = process_path
 app.config['ARCHIVED'] = archived_path
 # These are the extension that we are accepting to be uploaded
 app.config['ALLOWED_EXTENSIONS'] = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
@@ -42,7 +56,7 @@ def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
-def rename_files(folder_path): # IMPLEMENT YOUR OWN PROCESSING FUNCTION 
+def rename_files(folder_path, now): # IMPLEMENT YOUR OWN PROCESSING FUNCTION 
     # List all files in the specified folder
     files = os.listdir(folder_path)
 
@@ -53,7 +67,7 @@ def rename_files(folder_path): # IMPLEMENT YOUR OWN PROCESSING FUNCTION
         os.rename(os.path.join(folder_path, filename), os.path.join(folder_path, new_filename))
 
     parser = get_parser()
-    args = parser.parse_args(['--image', app.config['UPLOAD_FOLDER'], '--output', app.config['PROCESSED_FOLDER']])
+    args = parser.parse_args(['--image', 'uploads/' + now, '--output', 'processed/'+now])
     main(args)
 
 @app.route('/')
@@ -70,56 +84,57 @@ def mainframe():
 
 @app.route('/mainframe', methods = ['POST'])
 def upload_files():
+    now, upload_path, process_path = create_folder_and_update_path()
     files = request.files.getlist("file[]")
     for file in files:
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            file.save(os.path.join(upload_path, filename))
 
     # Rename files to cardinal numbers
-    rename_files(app.config['UPLOAD_FOLDER'])
+    rename_files(upload_path, now)
     
-    return redirect(url_for('process_uploads'))
+    return redirect(url_for(f'process_uploads', now=now))
 
-@app.route('/process_uploads')
-def process_uploads():
-    uploaded_files = os.listdir(app.config['UPLOAD_FOLDER'])
+@app.route('/process_uploads/<now>')
+def process_uploads(now):
+    uploaded_files = os.listdir('uploads/' + now)
     # file_count = len(uploaded_files)
-    return render_template('process_uploads.html', files=uploaded_files)
+    return render_template('process_uploads.html',now=now, files=uploaded_files)
 
-@app.route('/delete/<filename>', methods=['POST'])
-def delete_file(filename):
-    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    archive_path = os.path.join(app.config['ARCHIVED'], filename)
+@app.route('/delete/<now>/<filename>', methods=['POST'])
+def delete_file(now, filename):
+    file_path = os.path.join('uploads/' + now, filename)
+    archive_path = os.path.join(app.config['ARCHIVED'], filename) #should delete instead of archived
     if os.path.exists(file_path):
         os.replace(file_path, archive_path)
-    return redirect(url_for('process_uploads'))
+    return redirect(url_for('process_uploads', now = now))
 
 # Then, within your view function:
-@app.route('/view_file/<filename>')
-def view_file(filename):
+@app.route('/view_file/<now>/<filename>')
+def view_file(now, filename):
     safe_filename = secure_filename(filename)
-    file_path = safe_join(app.config['PROCESSED_FOLDER'],'final', safe_filename)
+    file_path = safe_join('processed/'+ now,'final', safe_filename)
     if os.path.exists(file_path):
         return send_file(file_path)
     else:
         return 'File not found', 404
     
-@app.route('/download', methods=['POST'])
-def download():
+@app.route('/download/<now>', methods=['POST'])
+def download(now):
     with ZipFile('translated.zip', 'w') as zipf:
-        for root, dirs, files in os.walk(os.path.join(app.config['PROCESSED_FOLDER'], 'final')):
+        for root, dirs, files in os.walk(os.path.join('processed/'+ now, 'final')):
             for file in files:
                 zipf.write(os.path.join(root, file), os.path.relpath(os.path.join(root, file), 'translated.zip'))
     return send_file('translated.zip', as_attachment=True)
 
-@app.route('/delete_upload', methods=['GET', 'POST'])
-def delete_upload():
+@app.route('/delete_upload/<now>', methods=['GET', 'POST'])
+def delete_upload(now):
     print("move")
     try:
-     files = os.listdir(app.config['UPLOAD_FOLDER'])
+     files = os.listdir('uploads/' + now)
      for file in files:
-       file_path = os.path.join(app.config['UPLOAD_FOLDER'], file)
+       file_path = os.path.join('uploads/' + now, file)
        archive_path = os.path.join(app.config['ARCHIVED'], file)
        if os.path.isfile(file_path):
          os.replace(file_path, archive_path)
@@ -127,7 +142,7 @@ def delete_upload():
      print("All files moved successfully.")
     except OSError:
      print("Error occurred while moving files.")
-    clear_folder(app.config['PROCESSED_FOLDER'])
+    clear_folder('processed/'+ now)
     # Code for the function you want to trigger goes here
     # For example, you can render a template
     return redirect(url_for('index'))
@@ -156,18 +171,16 @@ def clear_folder(folder_path):
             print(f"Failed to delete {item_path}. Reason: {e}")
 
 if __name__ == '__main__':
-    clear_folder(app.config['UPLOAD_FOLDER'])
-    clear_folder(app.config['PROCESSED_FOLDER'])
     #clear 1 day old process folder user and upload folder user
     for folder in os.listdir('uploads/'):
         if int(now[:8]) - int(folder[:8]) > 1:
             print(f'delete {folder}')
-            clear_folder('uploads/' + folder)
+            os.rmdir('uploads/'+ folder)
 
     for folder in os.listdir('processed/'):
         if int(now[:8]) - int(folder[:8]) > 1:
             print(f'delete {folder}')
-            clear_folder('processed/' + folder)
+            os.rmdir('processed/'+ folder)
     app.run() 
     # from waitress import serve
     # serve(app, host="0.0.0.0", port=80)
